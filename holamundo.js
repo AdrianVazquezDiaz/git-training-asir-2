b8a31e239e429072068b3d4c40185a3c96438d71
Adrián Vázquez <adrivazquezdiaz12@gmail.com>
2023-10-30 13:49:01 +0100

    js adapted to node, holamundo and fibonacci

--- holamundo.js
+++ holamundo.js
@@ -1,2 +1,8 @@
-let nombre= 'Miau '
-console.log('Holiwi' + nombre;)
+console.log ("Programa para imprimir la clase");
+const name = "ASIR2"
+let number = 5;
+
+for (let i = 0; i < number; i++) {
+	console.log(name);
+}
+
